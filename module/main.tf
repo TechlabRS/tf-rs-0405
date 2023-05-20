b0d2f67b46e7059209b54e25b7a5f12a61af526e
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-05-20 18:32:48 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -32,7 +32,7 @@ resource "null_resource" "provisioner" {
 
 resource "aws_route53_record" "records" {
   zone_id = "Z004850315D4679O2YUJ7"
-  name    = "${var.component_name}-dev.raghas.com"
+  name    = "${var.component_name}-dev.uknowme.tech"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
